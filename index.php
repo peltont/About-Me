79ad13423671c46d4ca65c211bb0064d349ce401
Trevor Pelton <tnpflyer20@msn.com>
2016-10-12 13:59:07 -0600

    Learning CSS2 navigation start

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
 	<body>
 		<div class="title">
 		<header class="main">Welcome to my Site</header>
+			<table>
+				<tr>
+					<th>Navigation</th>
+					<td>About Me</td>
+					<td>Resume</td>
+					<td>Projects</td>
+					<td>Contact Me</td>
+				</tr>
+			</table>
 		</div>
 		<br>
 		<h1>My name is Trevor Pelton, </h1>
