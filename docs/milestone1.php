06b29a28cc457da544a7c55de59f1ee8b88aa099
Trevor Pelton <tnpflyer20@msn.com>
2016-10-12 13:15:49 -0600

    Milestone 1 initial creation

---
+++ docs/milestone1.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 1</title>
+	</head>
+	<body>
+		<p>Purpose: I would like my personal website project to provide me with a professional looking personal website that prospective employers and clients can visit to learn about my and my capabilities.</p>
+		<p>Audience: My initial audience would consist of small to medium sized business owners. Eventually I'd like to expand upon both site to include projects I've been a part of and/created and other content geared towards my professional peers. </p>
+		<p>Initially my goal would be to find full time employment that I can use to gain further experience in my chosen field. </p>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Trevor's Page</title>
+	</head>
+	<body>
+		<header>Welcome to my Site</header>
+		<br>
+		<h1>My name is Trevor Pelton, </h1>
+
+	</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fushi
+ * Date: 10/11/2016
+ * Time: 4:25 PM
+ */
