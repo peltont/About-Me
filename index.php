82cfaf694e15ed80e2999c7229189fc3b5cdbb6e
Trevor Pelton <tnpflyer20@msn.com>
2016-10-17 13:15:26 -0600

    copypasta menu 2

--- index.php
+++ index.php
@@ -1,31 +1,48 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<style type="text/css">
+			ul {list-style: none;padding: 0px;margin: 0px;}
+			ul li {display: block;position: relative;float: left;border:1px solid #000}
+			li ul {display: none;}
+			ul li a {display: block;background: #000;padding: 5px 10px 5px 10px;text-decoration: none;
+				white-space: nowrap;color: #fff;}
+			ul li a:hover {background: #f00;}
+			li:hover ul {display: block; position: absolute;}
+			li:hover li {float: none;}
+			li:hover a {background: #f00;}
+			li:hover li a:hover {background: #000;}
+			#drop-nav li ul li {border-top: 0px;}
+		</style>
 		<link href="css/pwpstylesheet.css" rel="stylesheet" type="text/css">
 		<meta charset="UTF-8">
 		<title>Trevor's Page</title>
 	</head>
 	<body>
-		<div class="menu-wrap">
-			<nav class="menu">
-				<ul class="clearfix">
-					<li><a href="#">Home</a></li>
-					<li>
-						<a href="#">Navigation<span class="arrow">&#9660;</span></a>
-
-						<ul class="sub-menu">
-							<li><a href="#">About Me</a></li>
-							<li><a href="#">Previous Projects</a></li>
-							<li><a href="#">Resume</a></li>
-							<li><a href="#">Contact Me</a></li>
-						</ul>
-					</li>
-					<li><a href="#">T.V. Shows</a></li>
-					<li class="current-item"><a href="#">Photos</a></li>
-					<li><a href="#">Site Help</a></li>
+		<ul id="drop-nav">
+			<li><a href="#">Support</a></li>
+			<li><a href="#">Web Design</a>
+				<ul>
+					<li><a href="#">HTML</a></li>
+					<li><a href="#">CSS</a></li>
+					<li><a href="#">JavaScript</a></li>
 				</ul>
-			</nav>
-		</div>
+			</li>
+			<li><a href="#">Content Management</a>
+				<ul>
+					<li><a href="#">Joomla</a></li>
+					<li><a href="#">Drupal</a></li>
+					<li><a href="#">WordPress</a></li>
+					<li><a href="#">Concrete 5</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Contact</a>
+				<ul>
+					<li><a href="#">General Inquiries</a></li>
+					<li><a href="#">Ask me a Question</a></li>
+				</ul>
+			</li>
+		</ul>
 		<!--<div class="title">
 		<header class="main">Welcome to my Site</header>
 			</div>
