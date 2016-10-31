33a585513a8e64767e45369e838a92d7793d25d7
Trevor Pelton <tnpflyer20@msn.com>
2016-10-31 07:19:31 -0600

    initial milestone2

--- docs/milestone1.php
+++ docs/milestone1.php
@@ -5,6 +5,7 @@
 		<!---->
 	</head>
 	<body>
+		<h1>PWP Milestone 1</h1>
 		<p>Purpose: I would like my personal website project to provide me with a professional looking personal website that prospective employers and clients can visit to learn about my and my capabilities.</p>
 		<p>Audience: My initial audience would consist of small to medium sized business owners. Eventually I'd like to expand upon both site to include projects I've been a part of and/created and other content geared towards my professional peers. </p>
 		<ul>
---
+++ docs/milestone2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Milestone 2</title>
+	</head>
+	<body>
+		<p>
+			Blog: User can read about what I have been working on lately and up to date information on current projects.
+			<br>
+			About Me: Simple history of myself, education, work experience. Resume will be linked here.
+			<br>
+			Contact Me: Has a built in email message sender as well as links to my email, twitter, and github
+			<br>
+			Past Projects: Links and descriptions of all previous open source projects I have worked on.
+		</p>
+		<img src="/img/about-me-m.png">
+		<img src="/img/blog-m.png">
+		<img src="/img/contact-me-m.png">
+		<img src="/img/past-projects-m.png">
+		<br>
+		<img src="/img/about-me.png">
+		<img src="/img/Blog.png">
+		<img src="/img/Contact-me.png">
+		<img src="/img/Past-Projects.png">
+	</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fushi
+ * Date: 10/31/2016
+ * Time: 7:10 AM
+ */
